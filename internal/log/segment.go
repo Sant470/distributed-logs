9a03b06425035e067f34d00538336abcbadbf69d
Sant Purushottam <purushotam.iitr@gmail.com>
2024-11-13 01:43:12 +0530

    feat: read and write to segments

---
+++ internal/log/segment.go
@@ -0,0 +1,98 @@
+/*
+	segment wraps the index and store types to coordinate operations across the two.
+	When the log appends a record to the active segment, the segment needs to write the data to its store
+	and add a new entry in the index.
+	Similarly, for read, the segment needs to look up the entry from the index and then fetch the data from the store.
+*/
+
+package log
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/sant470/distlogs/api/v1"
+	"google.golang.org/protobuf/proto"
+)
+
+type segment struct {
+	store                  *store
+	index                  *index
+	baseOffset, nextOffset uint64
+	config                 Config
+}
+
+func newSegment(dir string, baseOffset uint64, c Config) (*segment, error) {
+	s := &segment{
+		baseOffset: baseOffset,
+		config:     c,
+	}
+	storeFile, err := os.OpenFile(
+		path.Join(dir, fmt.Sprintf("%d%s", baseOffset, ".store")),
+		os.O_RDWR|os.O_CREATE|os.O_APPEND,
+		0644,
+	)
+	if err != nil {
+		return nil, err
+	}
+	if s.store, err = newStore(storeFile); err != nil {
+		return nil, err
+	}
+	indexFile, err := os.OpenFile(
+		path.Join(dir, fmt.Sprintf("%d%s", baseOffset, ".index")),
+		os.O_RDWR|os.O_CREATE,
+		0644,
+	)
+	if err != nil {
+		return nil, err
+	}
+	if s.index, err = newIndex(indexFile, c); err != nil {
+		return nil, err
+	}
+	if off, _, err := s.index.Read(-1); err != nil {
+		s.nextOffset = baseOffset
+	} else {
+		s.nextOffset = baseOffset + uint64(off) + 1
+	}
+	return s, nil
+}
+
+func (s *segment) Append(record *api.Record) (offset uint64, err error) {
+	cur := s.nextOffset
+	record.Offset = cur
+	p, err := proto.Marshal(record)
+	if err != nil {
+		return 0, err
+	}
+	_, pos, err := s.store.Append(p)
+	if err != nil {
+		return 0, err
+	}
+	// index offset are relative to base offset
+	if err = s.index.Write(
+		uint32(s.nextOffset-uint64(s.baseOffset)),
+		pos,
+	); err != nil {
+		return 0, err
+	}
+	s.nextOffset++
+	return cur, nil
+}
+
+func (s *segment) Read(off uint64) (*api.Record, error) {
+	_, pos, err := s.index.Read(int64(off - s.baseOffset))
+	if err != nil {
+		return nil, err
+	}
+	p, err := s.store.Read(pos)
+	if err != nil {
+		return nil, err
+	}
+	record := &api.Record{}
+	err = proto.Unmarshal(p, record)
+	if err != nil {
+		return nil, err
+	}
+	return record, nil
+}
